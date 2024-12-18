0c80bd1a9f19791c9a78c1e545f4718ebf682327
Gracie B. <139829591+rorythenerd@users.noreply.github.com>
2024-12-17 21:00:23 -0500

    Create maths.py

---
+++ maths.py
@@ -0,0 +1,17 @@
+import math
+x = 81
+y = 5
+#print(math.acos(x))
+print(math.atan(x))
+print(math.atan2(y, x))
+print(math.ceil(x))
+print(math.cos(x))
+print(math.degrees(x))
+print(math.e)
+print(math.exp(x))
+print(math.factorial(x))
+print(math.floor(x))
+print(math.isnan(x))
+print(math.log(x, y))
+print(math.log2(x))
+print(math.pow(x, y))
