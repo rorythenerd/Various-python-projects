bfd4773a593253b4e2d1fb5e4b9074655ff69737
G B <139829591+rorythenerd@users.noreply.github.com>
2025-02-25 17:38:48 -0500

    Create bottles.py
    I changed the last lines, where there is one bottle left and then none

---
+++ bottles.py
@@ -0,0 +1,11 @@
+for i in range(99, 0, -1):
+  print(f'{i} bottles of beer on the wall ')
+  print(f'{i} bottles of beer')
+  print('Take one down, pass it around')
+  print(f'{i} bottles of beer on the wall')
+
+if i == 1:
+  print("1 bottle of beer on the wall")
+  print("1 bottles of beer")
+  print('Take one down, pass it around')
+  print('There are no more bottles left')
