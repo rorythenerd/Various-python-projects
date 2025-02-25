427cf770e892b038c48d5d92b5f15a4d8f3f5a81
G B <139829591+rorythenerd@users.noreply.github.com>
2025-02-25 17:47:32 -0500

    Update bottles.py

--- bottles.py
+++ bottles.py
@@ -2,7 +2,7 @@ for i in range(99, 0, -1):
   print(f'{i} bottles of beer on the wall ')
   print(f'{i} bottles of beer')
   print('Take one down, pass it around')
-  print(f'{i} bottles of beer on the wall')
+  print(f'{i-1} bottles of beer on the wall')
 
 if i == 1:
   print("1 bottle of beer on the wall")
